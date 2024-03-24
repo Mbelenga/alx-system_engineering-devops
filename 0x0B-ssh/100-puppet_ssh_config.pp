29f04549372974ec5b506dec5601cc5c9c398a7b
Mbelenga <mbelengaterence@gmail.com>
2024-03-24 14:22:07 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Client configuration file (w/ Puppet)
+# Setting up my client config file
+
+file_line { 'Turn off password auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
