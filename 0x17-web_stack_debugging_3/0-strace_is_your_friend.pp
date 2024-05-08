0c55d612f51efe13c9c7bd8aa87e5c67bf5348af
Mbelenga <mbelengaterence@gmail.com>
2024-05-08 14:52:29 +0300

    still an error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,6 @@
-Puppet
+# This Script Fixes the apache 500 error
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
